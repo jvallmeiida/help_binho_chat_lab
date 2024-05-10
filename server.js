12b14b1b2d39fa0ce8dd2755b38aeabb566a4aff
Joao Almeida <aalmeidadev@gmail.com>
2024-05-09 21:07:40 -0300

    WIP

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const routes = require('./routes');
+const app = express();
+
+app.use('/', routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
