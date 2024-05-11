e48bc6d98f61dd32ac269a583c010f01cf2f5dbd
Joao Almeida <aalmeidadev@gmail.com>
2024-05-11 13:24:55 -0300

    WIP

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,9 +1,22 @@
-const Product = require('../models/Product');
+const product = require('../models/Product');
+const productVariation = require('../models/ProductVariation');
 
 async function getAllProducts(req, res) {
   try {
-    const products = await Product.findAll();
-    res.json(products);
+    const products = await product.findAll();
+    const variations = await productVariation.findAll();
+    
+    const productsWithVariations = products.map(product => {
+      const productVariations = variations.filter(variation => variation.productId === product.id);
+      return {
+        id: product.id,
+        name: product.name,
+        prices: product.prices,
+        variations: productVariations
+      };
+    });
+
+    res.json(productsWithVariations);
   } catch (error) {
     console.error('Erro ao buscar produtos:', error);
     res.status(500).json({ error: 'Erro ao buscar produtos.' });
