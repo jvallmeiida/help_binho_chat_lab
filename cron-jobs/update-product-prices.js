d3778713d9790d9a9792f79b102ce82709c612e7
Joao Almeida <aalmeidadev@gmail.com>
2024-05-11 13:59:47 -0300

    WIP

---
+++ cron-jobs/update-product-prices.js
@@ -0,0 +1,17 @@
+const cron = require('node-cron');
+const { updateProductsPrices } = require('../services/updateProductPrices');
+
+const intervalMinutes = process.env.INTERVAL_MINUTES || 60; 
+
+cron.schedule(`*/${intervalMinutes} * * * *`, async () => {
+  try {
+    console.log('Executando atualização de preços dos produtos...');
+    await updateProductsPrices('iPhone 14');
+    await updateProductsPrices('Notebook');
+    await updateProductsPrices('Amazfit');
+
+    console.log('Atualização de preços concluída.');
+  } catch (error) {
+    console.error('Erro ao atualizar preços dos produtos:', error);
+  }
+});
